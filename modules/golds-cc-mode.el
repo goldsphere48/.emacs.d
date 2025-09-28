c83b397f1646fef9be6707af407b12ead36402cc
Viktor Titov <goldsphere48@gmail.com>
2025-09-28 14:26:17 +1000

    Fix indetation problems

--- modules/golds-cc-mode.el
+++ modules/golds-cc-mode.el
@@ -2,14 +2,27 @@
 (use-package clang-format
   :ensure t)
 
-(use-package cc-mode
-  :hook
-  (c-mode-common . 
-   (lambda ()
-     (c-set-style "linux")
-     (setq tab-width 4)
-     (setq c-basic-offset 4)
-     (setq indent-tabs-mode 't)
-     (c-set-offset 'inline-open 0))))
+(defun golds-c-style-setup ()
+  (c-set-style "bsd")
+  (setq-local indent-tabs-mode t)
+  (setq-local tab-width 4)
+  (setq-local c-basic-offset 4)
+
+  (c-set-offset 'defun-open 0)
+  (c-set-offset 'defun-close 0)
+  (c-set-offset 'block-open 0)
+  (c-set-offset 'block-close 0)
+  (c-set-offset 'substatement-open 0)
+
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'arglist-cont-nonempty '+)
+  (c-set-offset 'arglist-close 0)
+
+  (setq-local lsp-enable-indentation nil)
+  (setq-local lsp-enable-on-type-formatting nil)
+  (setq-local c-indent-empty-lines nil)
+)
+
+(add-hook 'c-mode-common-hook #'golds-c-style-setup)
 
 (provide 'golds-cc-mode)
